2458b59d6056f5b56cb46dd01249b9b40d4db93f
wallworkj <jwallwor@purdue.edu>
2026-04-10 14:40:21 -0400

    water method concepts

---
+++ src/water.cpp
@@ -0,0 +1,12 @@
+//given place, amount, flow
+//calculate radial bfs for initial flow, location
+//return initial water bottom mesh
+
+//pre-calculate path until rest or end of map 
+//return hashable, tiered, invisible mesh with spots for speed, acceleration, generated in steps
+
+//given edges, current volume, turbulence (vector)
+//limitations volumetric calculation
+//return top mesh(es) 
+
+
