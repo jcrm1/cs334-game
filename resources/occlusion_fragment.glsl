58a5f846efe20d8c94269292875f76c35ab77690
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2026-04-16 20:07:52 -0400

    make occlusion fragment shader write fog level

--- resources/occlusion_fragment.glsl
+++ resources/occlusion_fragment.glsl
@@ -6,7 +6,15 @@ in vec3 vColor;
 
 uniform vec3 cameraPos;
 uniform vec4 clearColor;
+uniform float fogStart;
+uniform float fogLength;
 
 void main() {
-  FragColor = vec4(0.0, 0.0, 0.0, 0.0); // black
+  float dist = distance(vPos, cameraPos);
+  if (dist > fogStart) {
+    float s = min((dist - fogStart) / fogLength, 1.0);
+    FragColor = vec4(s, s, s, 1.0);
+  } else {
+    FragColor = vec4(0.0, 0.0, 0.0, 0.0); // black
+  }
 }
