65c8173ecee71a2239d6ffdd2319ecaa19467cb9
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2026-04-16 20:07:59 -0400

    add fog to godrays

--- resources/godray_fragment.glsl
+++ resources/godray_fragment.glsl
@@ -2,12 +2,18 @@
 out vec4 FragColor;
 
 in vec2 screenLightPos;
+in vec3 vPos;
 
 uniform uint screenWidth;
 uniform uint screenHeight;
 uniform vec4 clearColor;
 uniform sampler2D prevTex;
 uniform sampler2D maskTex;
+uniform float fogStart;
+uniform float fogLength;
+// const float fogStart = 50.0;
+// const float fogLength = 25.0;
+uniform vec3 cameraPos;
 
 // godrays from https://developer.nvidia.com/gpugems/gpugems3/part-ii-light-and-shadows/chapter-13-volumetric-light-scattering-post-process
 
@@ -20,16 +26,26 @@ const float exposure = 1;
 void main() {
   vec2 texCoord = gl_FragCoord.xy / vec2(screenWidth, screenHeight);
   vec3 sunlight = vec3(0,0,0);
-  if (screenLightPos.x >= 0 && screenLightPos.x <= 1) {
+  if (screenLightPos.x >= 0 && screenLightPos.x <= 1 && texture(maskTex, texCoord).x == 1) {
     // Render the sun
     float d = 1 - clamp((distance(screenLightPos, texCoord) * 10), 0, 1);
     sunlight = vec3(d, d, d);
   }
-  if (screenLightPos.x >= 0 && texture(maskTex, texCoord).x == 0) {
+  float dist = distance(vPos, cameraPos);
+  vec3 sunPlusColor = sunlight + texture(prevTex, texCoord).xyz;
+  if (texture(maskTex, texCoord).x != 1 && screenLightPos.x >= 0) {
+    // terrain
+
+    // THE PROBLEM:
+    // The vPos I get from vertex shader is not the coordinate
+    // of the terrain vertex like I think it is. Rather, it is
+    // the coordinate of a vertex of the SCREEN SPACE TRIANGLE. AUGHHHHH
+    // edit: maybe fixed? leaving note for future reference
     vec2 deltaTexCoord = (texCoord - screenLightPos) * (density * (1.0 / numSamples));
     float illuminationDecay = 1.0;
     vec2 rayTexCoord = texCoord;
-    vec3 color = texture(maskTex, texCoord).xyz;
+    vec3 orig_mask = texture(maskTex, texCoord).xyz
+    vec3 color = orig_mask;
     for (int i = 0; i < numSamples; i += 1) {
       rayTexCoord -= deltaTexCoord;
       vec3 sample = texture(maskTex, rayTexCoord).xyz;
@@ -37,8 +53,9 @@ void main() {
       color += sample;
       illuminationDecay *= decay;
     }
-    FragColor = vec4(mix(sunlight + texture(prevTex, texCoord).xyz, (color * exposure), 0.2), 1.0);
+    FragColor = vec4(mix(mix(sunPlusColor, (color * exposure), 0.2), clearColor.xyz, orig_mask.x), 1.0);
   } else {
-    FragColor = vec4(sunlight + texture(prevTex, texCoord).xyz, 1.0);
+    // not terrain
+    FragColor = vec4(sunPlusColor, 1.0);
   }
 }
--- resources/godray_vertex.glsl
+++ resources/godray_vertex.glsl
@@ -1,20 +1,20 @@
 #version 330 core
 layout (location = 0) in vec2 aPos;
 
-const vec3 lightPos = vec3(200,100,0);
-
+uniform vec3 lightPos = vec3(200,100,0);
+uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform uint screenWidth;
 uniform uint screenHeight;
 // uniform vec3 lightPos;
 
+out vec3 vPos;
 out vec2 screenLightPos;
 
 void main()
 {
   gl_Position = vec4(aPos, 0.0, 1.0);
-
   vec4 clipPos = projection * view * vec4(lightPos, 1.0);
   if (clipPos.w <= 0.0) {
     screenLightPos = vec2(-1,-1);
@@ -24,4 +24,5 @@ void main()
     screenLightPos = ndc01;
     // screenLightPos = ndc01 * vec2(screenWidth, screenHeight);
   }
+  vPos = vec3(model * gl_Position);
 }
